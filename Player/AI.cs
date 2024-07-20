dddcea88e3a5d2e0e471a1579e7dbf79a95b7ea9
SirJoelingtonIII <32949172+SirJoelingtonIII@users.noreply.github.com>
2024-07-20 11:21:33 -0500

    Added descriptions of AI states

--- Player/AI.cs
+++ Player/AI.cs
@@ -37,9 +37,9 @@ public partial class AI : Player
 
 	enum AI_SUB_TOY_WITH_STATE
 	{
-		BE_IN_FRONT_OF, //Move to a Waypoint Player is moving towards
-		SHAPE_FLICKER, //Change sprite frame to nightmare for a frame or two flicker
-		//??
+		BE_IN_FRONT_OF, //Teleport to a Waypoint Player is moving towards
+		SHAPE_FLICKER, //Change sprite frame to nightmare for a frame or two flicker in in view port
+		MAKE_SOUND //Make a noise
 	}
 
 	enum AI_SUB_STAND_THERE_MENICINGLY_STATE
@@ -68,47 +68,174 @@ public partial class AI : Player
 		PATROL_GOAL_AREA, //Move near player goal
 		CATCH //Move to catch caught player
 	}
-	#endregion
-	#endregion
+    #endregion
+    #endregion
 
-	private AI_MAIN_BEHAVIOR_STATE currentMainState = AI_MAIN_BEHAVIOR_STATE.STAND_THERE_MENICINGLY; //Current AI State, start as do nothing 
-	private int currentSubState = 0; //Current AI Substate, start as do nothing. This is an int so I can cheat and use different enum types. ALWAYS CAST SUB STATE TO INT WHEN CHANGING IT
+    #region AI_STATE_FUNCS
 
-	//Teleport Function
+    //Current AI State
+    private AI_MAIN_BEHAVIOR_STATE currentMainState = AI_MAIN_BEHAVIOR_STATE.STAND_THERE_MENICINGLY; //Current AI State, start as do nothing 
+	private int currentSubState = 0; //Current AI Substate, start as do nothing. This is an int so I can cheat and use different enum types. ALWAYS CAST SUB STATE TO INT WHEN CHANGING OR REFERENCING IT
 
-	private void AI_chaseState()
+    //Stored AI state for toy_with states to switch to and from to
+    private AI_MAIN_BEHAVIOR_STATE storeMainState = AI_MAIN_BEHAVIOR_STATE.STAND_THERE_MENICINGLY;
+    private int storeSubState = 0;
+
+    //Time since last toy with occured
+    private double toyWithTimer = 0.0; 
+
+    //Timers for behavior logic
+	private double timeIdle = 0.0;
+    private double timeMoving = 0.0;
+
+    private double timeInMainBehavior = 0.0;
+    private double timeInSubBehavior = 0.0;
+
+    //Waypoint of interest
+    private int currentWaypointID = -1;
+    
+
+
+    private void AI_chaseState()
 	{
-		//TODO, Handle State
-	}
+		switch (currentSubState) 
+		{
+			case (int)AI_SUB_CHASE_STATE.LOCATE:
+				//Stand there for a bit, reference timeIdle
+				break;
+
+            case (int)AI_SUB_CHASE_STATE.PURSUE:
+                //Nav to player point directly using nav agent
+                break;
+
+            case (int)AI_SUB_CHASE_STATE.CHARGE:
+                //Lunge at player's current location
+                break;
+
+            case (int)AI_SUB_CHASE_STATE.BLOCKED:
+                //AI Can't Nav to player point directly
+                break;
+
+            case (int)AI_SUB_CHASE_STATE.JUKED:
+                //Did not catch with charge, act confused for a moment, then go
+                break;
+
+        }
+    }
 
     private void AI_guardState()
     {
-        //TODO, Handle State
+        switch (currentSubState)
+        {
+            case (int)AI_SUB_GUARD_STATE.MOVE_TO_GOAL:
+                //Nav to Goal
+                break;
+
+            case (int)AI_SUB_GUARD_STATE.PATROL_GOAL_AREA:
+                //Move in Goal Area
+                break;
+
+            case (int)AI_SUB_GUARD_STATE.CATCH:
+                //Lunge at player's current location, like chase.charge
+                break;
+
+        }
     }
 
     private void AI_impedeState()
     {
-        //TODO, Handle State
+        switch (currentSubState)
+        {
+            case (int)AI_SUB_IMPEDE_STATE.GET_CLOSE:
+                //Nav to general area of player
+                break;
+
+            case (int)AI_SUB_IMPEDE_STATE.BLOCK_PATH:
+                //Nav to general point in front player
+                break;
+
+            case (int)AI_SUB_IMPEDE_STATE.ADJUST:
+                //Move in opposite direction of player for a bit. If you want to catch, change to chase.charge
+                break;
+
+
+        }
     }
 
     private void AI_pounceState()
     {
-        //TODO, Handle State
+        switch (currentSubState)
+        {
+            case (int)AI_SUB_POUNCE_STATE.ASSUME_POSITION:
+                //Stand there out of frame until player is out of healing state
+                break;
+
+            case (int)AI_SUB_POUNCE_STATE.YEET:
+                //Jump the Player
+                break;
+        }
     }
 
     private void AI_standThereMenicinglyState()
     {
-        //TODO, Handle State
+        switch (currentSubState)
+        {
+            case (int)AI_SUB_STAND_THERE_MENICINGLY_STATE.BE_SPOOKY:
+                //Stand there for a bit, reference timeIdle
+                break;
+
+            case (int)AI_SUB_STAND_THERE_MENICINGLY_STATE.MOVE_TO_SPOT:
+                //Nav to waypoint to stand menicingly
+                break;
+
+        }
     }
 
     private void AI_toyWithState()
     {
-        //TODO, Handle State
+        switch (currentSubState)
+        {
+            case (int)AI_SUB_TOY_WITH_STATE.BE_IN_FRONT_OF:
+                //Teleport to point infront of player offscreen
+                break;
+
+            case (int)AI_SUB_TOY_WITH_STATE.SHAPE_FLICKER:
+                //Change Sprite for a frame or two while on screen
+                break;
+
+            case (int)AI_SUB_TOY_WITH_STATE.MAKE_SOUND:
+                //Play Audio clip
+                break;
+
+
+        }
     }
 
     private void AI_wanderState()
     {
-        //TODO, Handle State
+        switch (currentSubState)
+        {
+            case (int)AI_SUB_WANDER_STATE.STAY_OUT_OF_VIEW:
+                //Nav to Closest valid point not in the view port
+                break;
+
+            case (int)AI_SUB_WANDER_STATE.PASS_BY_VIEW:
+                //Nav to point on edge of player view port
+                break;
+
+            case (int)AI_SUB_WANDER_STATE.STAND_NEAR_GOAL:
+                //Nav near a goal waypoint and stand near it
+                break;
+
+            case (int)AI_SUB_WANDER_STATE.NAV_TO_RANDOM_WAYPOINT:
+                //Go to a random way point
+                break;
+
+            case (int)AI_SUB_WANDER_STATE.WALK_NEAR_WAYPOINT:
+                //Walk in area a distance away from the waypoint
+                break;
+
+        }
     }
 
     private void handleMainAIState() //Run every frame to handle the main state and handle the current input of the AI Player
@@ -149,6 +276,13 @@ public partial class AI : Player
 
         }
     }
+    #endregion
+
+    //Teleport Function
+	private void AI_teleportToLocation()
+	{
+		//Just blink the AI to target location
+	}
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
